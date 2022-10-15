b9d1daf08966d4fed0ce40ead6bd926a726aaeb9
iQuigg <115356856+iQuigg@users.noreply.github.com>
2022-10-15 14:39:13 +0200

    bankingSystemFinished

--- bankingSystem.cpp
+++ bankingSystem.cpp
@@ -1,3 +1,4 @@
+// (Run by Ctrl/Alt/N)
 
         /* Headers */
 
@@ -112,7 +113,7 @@ int main(){
     do
     {
         system("cls");
-        cout << "\n\n\n\tMAIN MENU";
+        cout << "\n\n\n\tMAIN MENU";        // \t= Tab
         cout << "\n\n\t01. NEW ACCOUNT";
         cout << "\n\n\t02. DEPOSIT AMOUNT";
         cout << "\n\n\t03. WITHDRAW AMOUNT";
@@ -204,3 +205,146 @@ void display_sp(int n)
 }
 
         /* Function to modify record of file */
+
+void modify_account(int n)
+{
+    bool found=false;
+    account ac;
+    fstream File;
+    File.open("account.dat", ios::binary|ios::in|ios::out);
+    if(!File)
+    {
+        cout << "File could not be opened! Press any Key...";
+        return;
+    }
+    while(!File.eof() && found==false)
+    {
+            File.read(reinterpret_cast < char *> (&ac), sizeof(account));
+            if(ac.retacno()==n)
+            {
+                    ac.show_account();
+                    cout << "\n\nEnter The New Details of your Account" << endl;
+                    ac.modify();
+                    int pos=(-1) * static_cast <int> (sizeof(account));
+                    File.seekp(pos,ios::cur);
+                    File.write(reinterpret_cast < char *> (&ac), sizeof(account));
+                    cout << "\n\n\t Record Updated";
+                    found=true;
+            }
+    }
+    File.close();
+    if(found==false)
+        cout << "\n\n Record Not Found";
+}
+
+        /* Function to delete record of files */
+
+void delete_account(int n)
+{
+	account ac;
+	ifstream inFile;
+	ofstream outFile;
+	inFile.open("account.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"File could not be opened! Press any Key...";
+		return;
+	}
+	outFile.open("Temp.dat",ios::binary);
+	inFile.seekg(0,ios::beg);
+	while(inFile.read(reinterpret_cast<char *> (&ac), sizeof(account)))
+	{
+		if(ac.retacno()!=n)
+		{
+			outFile.write(reinterpret_cast<char *> (&ac), sizeof(account));
+		}
+	}
+	inFile.close();
+	outFile.close();
+	remove("account.dat");
+	rename("Temp.dat","account.dat");
+	cout<<"\n\n\tRecord Deleted...";
+}
+
+        /* Function to display all accounts deposit list */
+
+void display_all()
+{
+    account ac;
+    ifstream inFile;
+    inFile.open("account,dat", ios::binary);
+    if(!inFile)
+    {
+        cout << "File could not be opened! Press any key...";
+        return;
+    }
+    cout << "\n\n\t\tACCOUNT HOLDER LIST\n\n";
+    cout << "====================================================\n";
+    cout << "A/c no.      NAME           Type  Balance\n";
+    cout << "====================================================\n";
+    while(inFile.read(reinterpret_cast < char *> (&ac), sizeof(account)))
+    {
+        ac.report();
+    }
+    inFile.close();
+}
+
+        /* Function to deposit and withdraw amounts */
+
+void deposit_withdraw(int n, int option)
+{
+    int amt;
+    bool found=false;
+    account ac;
+    fstream File;
+    File.open("account.dat", ios::binary|ios::in|ios::out);
+    if(!File)
+    {
+        cout << "File could not be opened! Press any key...";
+        return;
+    }
+    while(!File.eof() && found==false)
+    {
+        File.read(reinterpret_cast < char *> (&ac), sizeof(account));
+        if(option==1)
+        {
+            cout << "\n\n\tTO DEPOSIT AMOUNT ";
+            cout << "\n\nEnter The Amount to be deposited";
+            cin >> amt;
+            ac.dep(amt);
+        }
+        if(option==2)
+        {
+            cout << "\n\n\tWITHDRAW AMOUNT ";
+            cout << "\n\nEnter The Amount to ne withdrawn";
+            cin >> amt;
+            int bal=ac.retdeposit()-amt;
+            if((bal<500 && ac.rettype()=='S') || (bal<1000 && ac.rettype()=='C'))
+                cout << "Insufficience balance";
+            else
+                ac.draw(amt);
+        }
+        int pos=(-1)*static_cast <int> (sizeof(ac));
+        File.seekp(pos, ios::cur);
+        File.write(reinterpret_cast < char *> (&ac), sizeof(account));
+        cout << "\n\n\t Record Updated";
+        found=true;
+    }
+    File.close();
+    if(found==false)
+        cout << "\n\n Record not Found";
+}
+
+        /* Introduction Function*/
+
+void intro()
+{
+    cout << "\n\n\n\t BANK";
+    cout << "\n\n\tMANAGEMENT";
+    cout << "\n\n\t SYSTEM";
+    cout << "\n\n\n\nMADE BY: Karl-Christian";
+    cout << "\n\nSCHOOL: Gymnasium Papenburg";
+    cin.get();
+}
+
+        /* End of*/
