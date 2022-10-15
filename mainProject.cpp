8995f45410145799cc707c9d038cdffa133d72c1
iQuigg <115356856+iQuigg@users.noreply.github.com>
2022-10-15 10:36:53 +0200

    projects

---
+++ mainProject.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+    using namespace std;
+
+
+int main(){
+
+    string op;
+
+    cout << "This is a test formular, just to memorise everything I have done to this day." << '/n';
+    cin >> "What would you like to do? Select either: Math,  ";
+
+}
